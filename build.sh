d8781a0e6288e5ee93459f3fc5f8ff75bfbea81e
KevinFham <77748434+KevinFham@users.noreply.github.com>
2023-01-15 17:42:03 -0600

    opencv4.4.0 and Pangolin dependency additions

--- build.sh
+++ build.sh
@@ -1,10 +1,32 @@
+echo "Configuring and building Thirdparty/opencv4.4.0"
+
+cd Thirdparty/opencv4.4.0
+mkdir build
+cd build
+cmake .. -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON
+make -j4
+make install
+ldconfig
+
+cd ../../Pangolin
+
+echo "Configuring and building Thirdparty/Pangolin"
+
+mkdir build
+cd build
+cmake ..
+cmake --build .
+make -j4
+make install
+
+cd ../../DBoW2
+
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
-cd Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j4
 
 cd ../../g2o
 
@@ -13,7 +35,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j4
 
 cd ../../Sophus
 
@@ -22,7 +44,7 @@ echo "Configuring and building Thirdparty/Sophus ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j4
 
 cd ../../../
 
